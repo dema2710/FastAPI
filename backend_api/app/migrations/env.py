7618c3b9c1439f65283894577a8b3fad3aee70cd
demch <md2710@ukr.net>
2025-05-13 19:33:40 +0300

    codestyle

--- backend_api/app/migrations/env.py
+++ backend_api/app/migrations/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
 from database.base_models import Base
 from settings import settings
 

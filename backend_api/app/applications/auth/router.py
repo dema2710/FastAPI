073e34e322081053751cf7f954528acd7d18723d
demch <md2710@ukr.net>
2025-05-23 19:55:56 +0300

    make check1

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.auth.auth_handler import auth_handler
@@ -9,11 +8,10 @@ from applications.users.models import User
 from applications.users.schemas import BaseUserInfo
 from database.sessions_dependencies import get_async_session
 
-
 router_auth = APIRouter()
 
 
-@router_auth.post('/login')
+@router_auth.post("/login")
 async def user_login(
     data: OAuth2PasswordRequestForm = Depends(),
     session: AsyncSession = Depends(get_async_session),
@@ -23,7 +21,5 @@ async def user_login(
 
 
 @router_auth.get("/get-my-info")
-async def get_my_info(
-    user: User = Depends(get_current_user)
-) -> BaseUserInfo:
+async def get_my_info(user: User = Depends(get_current_user)) -> BaseUserInfo:
     return user

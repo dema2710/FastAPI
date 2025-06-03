68e07a49f39a8fc80f92ff19ed3e359f75dcc480
demch <md2710@ukr.net>
2025-06-03 21:26:09 +0300

    register1

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import Request
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.auth.password_handler import PasswordEncrypt
-from applications.users.crud import create_user_in_db
+from applications.users.crud import create_user_in_db, get_user_by_email
 from applications.users.models import User
 from applications.users.schemas import BaseFields, RegisterUserFields
 from database.sessions_dependencies import get_async_session
@@ -12,5 +13,9 @@ router_users = APIRouter()
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+    user = await get_user_by_email(new_user.email, session)
+    if user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
+
     await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
     return new_user

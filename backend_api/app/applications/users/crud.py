7d9174899178f39276085c8335197a3625e1e206
demch <md2710@ukr.net>
2025-06-06 20:26:16 +0300

    migrate

--- backend_api/app/applications/users/crud.py
+++ backend_api/app/applications/users/crud.py
@@ -1,18 +1,22 @@
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from applications.auth.password_handler import PasswordEncrypt
 from applications.users.models import User
 
 
-async def create_user_in_db(email, name, password, session: AsyncSession):
+async def create_user_in_db(email, name, password, session: AsyncSession) -> User:
     hashed_password = await PasswordEncrypt.get_password_hash(password)
     new_user = User(email=email, hashed_password=hashed_password, name=name)
     session.add(new_user)
+
     await session.commit()
+    # await session.refresh(new_user)
+    return new_user
 
 
-async def get_user_by_email(email, sessions: AsyncSession) -> User | None:
+
+async def get_user_by_email(email, session: AsyncSession) -> User | None:
     query = select(User).filter(User.email == email)
-    result = await sessions.execute(query)
+    result = await session.execute(query)
     return result.scalar_one_or_none()
--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,21 +1,21 @@
-from fastapi import Request
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from applications.auth.password_handler import PasswordEncrypt
 from applications.users.crud import create_user_in_db, get_user_by_email
-from applications.users.models import User
-from applications.users.schemas import BaseFields, RegisterUserFields
+from applications.users.schemas import BaseUserInfo, RegisterUserFields
 from database.sessions_dependencies import get_async_session
 
 router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseUserInfo:
     user = await get_user_by_email(new_user.email, session)
     if user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
 
-    await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
-    return new_user
+    created_user = await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
+
+
+
+    return created_user
